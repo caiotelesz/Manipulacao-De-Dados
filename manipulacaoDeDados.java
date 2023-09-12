03c83b8dfe047497eb172efd7f35b522134817b1
Caio Teles <103958122+caiotelesz@users.noreply.github.com>
2023-09-12 14:01:06 -0300

    Atualizando Manipulação de Dados

--- manipulacaoDeDados.java
+++ manipulacaoDeDados.java
@@ -6,6 +6,7 @@ public class VetorObjeto {
         this.tamanho = 0;
     }
 
+//    Adicionar elementos
     public void adiciona(Object elemento) throws Exception {
         this.aumentaCapacidade();
         if (this.tamanho < this.elementos.length) {
@@ -16,10 +17,12 @@ public class VetorObjeto {
         }
     }
 
+//    Tamanho
     public int tamanho() {
         return this.tamanho;
     }
 
+//   toString
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -39,6 +42,7 @@ public class VetorObjeto {
         return s.toString();
     }
 
+//    Buscar pela posição e entregará o nome
     public Object busca (int posicao) throws Exception {
         if (posicao >= 0 && posicao < tamanho) {
             return elementos[posicao];
@@ -47,6 +51,7 @@ public class VetorObjeto {
         }
     }
 
+//   Buscar pelo nome e entregará a posição
     public int buscaPos(Object elemento) {
         for(int i = 0; i < tamanho; i++) {
             if (elementos[i].equals(elemento)) {
@@ -56,6 +61,7 @@ public class VetorObjeto {
         return -1;
     }
 
+//    Adicionar no inicio do vetor
     public boolean adicionarInicio(int posicao, Object elementos) throws Exception {
         this.aumentaCapacidade();
         if (posicao >= 0 && posicao < tamanho) {
@@ -73,6 +79,7 @@ public class VetorObjeto {
         return true;
     }
 
+//    Aumentar a capacidade do vetor, quando chegar na capacidade colocada, automaticamente se mutiplicará
     private void aumentaCapacidade() {
         if(this.tamanho == this.elementos.length) {
             Object[] elementosNovos = new Object[this.elementos.length * 2];
@@ -82,7 +89,8 @@ public class VetorObjeto {
             this.elementos = elementosNovos;
         }
     }
-
+    
+//    Remover elementos
     public void remove(int posicao) throws Exception {
         if(posicao >= 0 && posicao < tamanho) {
             for (int i = posicao; i<this.tamanho-1; i++) {
